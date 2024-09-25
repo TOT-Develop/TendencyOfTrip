bdb8924226f01ac973535f687cafdb4ecb2c0a77
onexeuo <168074915+onexeuo@users.noreply.github.com>
2024-09-25 09:13:08 +0900

    Delete totone directory

--- totone/src/main/webapp/static/js/adminMenu.js
+++
@@ -1,8 +0,0 @@
-window.onload = function (){
-	$("#login").on('click', function(){
-		window.location.href="adminLogin.jsp";
-	})	
-	$("#user").on('click', function(){
-		window.location.href="adminUser.jsp";
-	})
-}
